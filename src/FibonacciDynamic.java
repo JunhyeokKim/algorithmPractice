1b7aa91fe9305b76835bd38e5dc2c144eb312cda
junhyeok <junhyeok.kim0506@gmail.com>
2016-10-20 15:17:25 +0900

    Version 1.0.0

--- src/FibonacciDynamic.java
+++ src/FibonacciDynamic.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by junhyeok on 2016-10-20.
  */
 public class FibonacciDynamic {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner("input.txt");
+        int T;
+        int n;
+        int zeroCnt, oneCnt;
+        T=sc.nextInt();
+
+        for(int t=0; t<T; t++){
+            zeroCnt=0;
+            oneCnt=0;
+            n=sc.nextInt();
 
 
+            System.out.println(String.format("%d %d",zeroCnt,oneCnt));
+        }
+
+    }
+    static int fibonacci(int n){
+
+        return 0;
+    }
 }
