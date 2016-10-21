1fd25038cd2dda6c81a4ade1c1b6c0d4fb53be97
junhyeok <junhyeok.kim0506@gmail.com>
2016-10-21 22:07:05 +0900

    Dynamic Fibonacci complete

---
+++ src/RGBDynamic.java
@@ -0,0 +1,87 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by junhyeok on 2016-10-20.
+ */
+public class RGBDynamic {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileInputStream("input2.txt"));
+        //Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] rgb;
+        int[] cost;
+        int[] table;        //r:0, g:1, b:2
+        int result;
+        rgb = new int[n][3];
+        table = new int[n];
+        cost = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rgb[i][0] = sc.nextInt();
+            rgb[i][1] = sc.nextInt();
+            rgb[i][2] = sc.nextInt();
+            table[i] = -1;
+
+        }
+
+
+        System.out.println(cost[n - 1]);
+    }
+
+
+    static int findMin(int[] rgb) {
+        int minIdx = 0;
+        if (rgb[0] > rgb[1]) {
+            minIdx = 1;
+            if (rgb[1] > rgb[2]) {
+                minIdx = 2;
+            }
+        } else if (rgb[0] > rgb[2]) {
+            minIdx = 2;
+            if (rgb[2] > rgb[1]) {
+                minIdx = 1;
+            }
+        }
+        return minIdx;
+    }
+
+
+    static int calCost(int[][] rgb, int[]cost, int[]table, int n){
+        if(n==0){
+            return 0;
+        }
+        else if(n==1&& table[0]==-1){
+            table[0]=findMin(rgb[0]);
+            cost[0]=rgb[0][table[0]];
+            return cost[0];
+        }
+        else if(n==2 && table[1]==-1){
+            table[1]=findMin(rgb[1]);
+            if(table[0]==table[1]){
+                int []temp0= rgb[0];
+                int []temp1=rgb[1];
+                temp0[table[0]]=Integer.MAX_VALUE;
+                temp1[table[1]]=Integer.MAX_VALUE;
+                int temp0Idx=findMin(temp0);
+                int temp1Idx=findMin(temp1);
+                if(rgb[0][table[0]]+rgb[1][temp1Idx]>rgb[0][temp0Idx]+rgb[1][table[1]]){
+                    table[0]=temp0Idx;
+                }
+                else{
+                    table[1]=temp1Idx;
+                }
+                cost[1]=rgb[0][table[0]]+rgb[1][table[1]];
+                return cost[1];
+            }
+        }
+        else{
+
+        }
+        return 0;
+    }
+
+}
+
---
+++ src/TriangleMaxSumDynamic.java
@@ -0,0 +1,75 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by junhyeok on 2016-10-21.
+ */
+public class TriangleMaxSumDynamic {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileInputStream("input3.txt"));
+        //Scanner sc= new Scanner(System.in);
+        for (int t = 1; t <= 1; t++) {
+            //int h = t;
+            int h = sc.nextInt();
+
+            int[] cost = new int[h];
+            int[][] arr = new int[h][];
+            int[] maxIdx = new int[h];
+            for (int i = 0; i < h; i++) {
+                arr[i] = new int[i + 1];
+                for (int j = 0; j <= i; j++) {
+                    //arr[i][j] = (int)(Math.random()*100);
+                    arr[i][j] = sc.nextInt();
+
+                }
+                maxIdx[i] = -1;
+            }
+            System.out.println(getCost(maxIdx, arr, cost, h - 1));
+
+
+        }
+    }
+
+    static int getCost(int[] maxIdx, int[][] arr, int[] cost, int h) {
+        for (int n = 0; n <= h; n++) {
+            if (n == 0) {
+                cost[0] = arr[0][0];
+                maxIdx[0] = 0;
+            } else if (n == 1) {
+                cost[1] = cost[0] + Math.max(arr[1][0], arr[1][1]);
+                maxIdx[1] = (arr[1][0] > arr[1][1]) ? 0 : 1;
+            } else {
+
+                int left = cost[n - 2] + arr[n - 1][maxIdx[n - 2]] + Math.max(arr[n][maxIdx[n - 2]], arr[n][maxIdx[n - 2] + 1]);
+                int right = cost[n - 2] + arr[n - 1][maxIdx[n - 2] + 1] + Math.max(arr[n][maxIdx[n - 2] + 1], arr[n][maxIdx[n - 2] + 2]);
+                if (left > right) {
+                    cost[n] = left;
+                    maxIdx[n - 1] = maxIdx[n - 2];
+                    cost[n - 1] = cost[n - 2] + arr[n - 1][maxIdx[n - 1]];
+                    if (arr[n][maxIdx[n - 2]] > arr[n][maxIdx[n - 2] + 1]) {
+                        maxIdx[n] = maxIdx[n - 2];
+                    } else {
+                        maxIdx[n] = maxIdx[n - 2] + 1;
+
+                    }
+
+                } else {
+                    cost[n] = right;
+                    maxIdx[n - 1] = maxIdx[n - 2] + 1;
+                    cost[n - 1] = cost[n - 2] + arr[n - 1][maxIdx[n - 1]];
+                    if (arr[n][maxIdx[n - 2] + 1] > arr[n][maxIdx[n - 2] + 2]) {
+                        maxIdx[n] = maxIdx[n - 2] + 1;
+                    } else {
+                        maxIdx[n] = maxIdx[n - 2] + 2;
+                    }
+
+                }
+            }
+        }
+        return cost[h];
+    }
+
+
+}
