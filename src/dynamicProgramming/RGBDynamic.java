1752e72f8e7771248226dc87a45a74a18a0c6177
jj <kjho1e3@naver.com>
2017-07-20 00:33:31 +0900

    좀 추가

--- src/dynamicProgramming/RGBDynamic.java
+++ src/dynamicProgramming/RGBDynamic.java
@@ -1,7 +1,8 @@
 package dynamicProgramming;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by junhyeok on 2016-10-20.
@@ -9,15 +10,12 @@ import java.util.Scanner;
 public class RGBDynamic {
     public static int[][] cost;
     public static int[] color;
-    public static int[][] map;
 
     public static void main(String[] args) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("rgb.txt")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         cost = new int[n + 1][3];
         color = new int[n + 1];
-        map = new int[n + 1][3];
         for (int i = 1; i <= n; i++) {
             String[] costs = br.readLine().split(" ");
             for (int j = 0; j < costs.length; j++) {
@@ -32,23 +30,17 @@ public class RGBDynamic {
     public static int rgb(int s, int e) {
         if (s > e) {
             return 0;
-        } else if(map[s][color[s]]!=0){
-            return map[s][color[s]];
-        }
-        else {
+        } else {
             int min = Integer.MAX_VALUE;
-            int minColor = -1;
             for (int i = 0; i < 3; i++) {   // i= color
                 if (color[s - 1] != i) {
                     color[s] = i;
                     int temp = cost[s][color[s]] + rgb(s + 1, e);
                     if (min > temp) {
                         min = temp;
-                        minColor = i;
                     }
                 }
             }
-            map[s][minColor] = min;
             return min;
         }
     }
