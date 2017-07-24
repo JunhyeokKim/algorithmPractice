a0b8f031cad9096570eb65df4ecdd157398b62c6
JunhyeokKim <junhyeok.kim0506@gmail.com>
2017-07-24 18:53:10 +0900

    ll

--- src/string/CountWord.java
+++ src/string/CountWord.java
@@ -3,26 +3,45 @@ package string;
 import java.io.*;
 
 /**
- * Created by junhyeok on 2017-07-23.
- * Problem No:1152
+ * Created by acorn on 2017-07-24.
  */
 public class CountWord {
     public static void main(String[] args) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("countWord.txt")));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String str= "The Curious Case of Benjamin Button";
-        //String str = br.readLine().trim();
-        boolean flag = false;
-        int cnt = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ' ' && !flag) {
-                cnt++;
-                flag = true;
-            } else if (str.charAt(i) == ' ' && flag) {
-                flag = false;
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("countWord.txt")));
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str = br.readLine().toCharArray();
+        int[] count = new int['z' - 'a'+1];
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] < 91) { // upper case
+                count[str[i] - 65]++;
+            } else {  //lower case
+                count[str[i] - 97]++;
             }
         }
-        System.out.println(cnt);
+        int max1 = 0;
+        int maxIdx1 = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (max1 < count[i]) {
+                max1 = count[i];
+                maxIdx1 = i;
+            }
+        }
+        int tempMax1=count[maxIdx1];
+        count[maxIdx1] = 0;
+        int max2 = 0;
+        int maxIdx2 = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (max2 < count[i]) {
+                max2 = count[i];
+                maxIdx2 = i;
+            }
+        }
+        if(tempMax1==count[maxIdx2]){
+            System.out.println("?");
+        }else{
+            System.out.println((char)(maxIdx1+65));
+        }
+
     }
+
 }
