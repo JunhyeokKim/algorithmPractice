8fb1d39b4ad9d8e3d20802762bc4ff523b3cd797
JunhyeokKim <junhyeok.kim0506@gmail.com>
2017-08-18 18:05:22 +0900

    ll

--- src/dynamicProgramming/ACM.java
+++ src/dynamicProgramming/ACM.java
@@ -4,32 +4,40 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Created by acorn on 2017-08-17.
+ * Created by jhKim on 2017-08-17.
+ * Problem No: 1005
+ * ACM:
+ * memoization 과 dfs를 활용하여 풀 수 있거나, 혹은 위상 정렬을 통해 풀 수 있다.
+ * 성능상에서는 위상정렬은 모든 정점들을 모두 방문해야 하기 때문에 도착점에서 dfs를 수행하는 것이 성능에서는 더 좋다.
+ * 위상 정렬을 활용한 방법
+ * 1. 각 노드의 indegree의 개수를 count한다.
+ * 2. 위상정렬을 수행한다. 먼저 indegree가 0인 정점들을 큐에 삽입한 뒤,
+ * 그 노드들을 방문하면서 outdegree로 연결된 outnode의 indegree의 개수를 감소시키고, 진출하는 간선을 제거한다.
+ * outNode에 대해서 indegree를 감소시킬 때, d[outNode]<d[node]+cost[outNode]를 비교하여 최대값을 갖는 것으로 갱신한다.
+ * 이를 큐가 빌때까지 반복
  */
 public class ACM {
-    static int[] cost = new int[1001];
-    static long[] d = new long[1001];
-    static ArrayList<Integer>[] adjList = new ArrayList[1001];
-    static Queue<Integer> queue = new LinkedList<>();
-    static int[] indegree = new int[1001];
-    private static int n;
-    private static int k;
+    static int[] cost;
+    static ArrayList<Integer>[] adjList;
+    static int[] dis;
+    /*static int[] indegree = new int[1001];*/
 
     public static void main(String[] args) throws IOException {
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input/acm.txt")));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder bd = new StringBuilder();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(st.nextToken());
-        int goal;
         for (int i = 0; i < T; i++) {
-            st= new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            Arrays.fill(d,Integer.MAX_VALUE);
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            cost = new int[n + 1];
+            adjList = new ArrayList[n + 1];
+            dis = new int[n + 1];
+            Arrays.fill(dis, -1);
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
                 cost[j] = Integer.parseInt(st.nextToken());
             }
@@ -37,55 +45,69 @@ public class ACM {
                 st = new StringTokenizer(br.readLine());
                 int prev = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
+                /*
+                위상정렬을 통한 방법
                 if (adjList[prev] == null) {
-                    adjList[prev] = new ArrayList<>();
+                    adjList[prev] = new LinkedList<>();
                 }
                 adjList[prev].add(to);
-                indegree[to]++;
+                indegree[to]++;*/
+                if (adjList[to] == null) {
+                    adjList[to] = new ArrayList<>();
+                }
+                adjList[to].add(prev);
             }
-            goal = Integer.parseInt(br.readLine());
-            topologicalSort();
+            int goal = Integer.parseInt(br.readLine());
+            /*bd.append(topologicalSort(goal, n) + "\n");*/
+            System.out.println(findDfs(goal));
         }
+        System.out.println(bd.toString());
 
 
     }
 
-    private static void topologicalSort() {
-        Queue<Integer> noIndegrees = new LinkedList<>();
+    private static int findDfs(int x) {
+        if (dis[x] != -1) {
+            return dis[x];
+        } else {
+            int res = 0;
+            if (adjList[x] != null) {
+                for (int adjNode : adjList[x]) {
+                    res = Math.max(res, findDfs(adjNode));
+                }
+            }
+            res += cost[x];
+            return dis[x] = res;
+        }
+    }
+
+    /*private static long topologicalSort(int goal, int n) {
+        long[] d = new long[1001];
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             if (indegree[i] == 0) {
-                noIndegrees.add(i);
+                // 초기 indegree가 0인 node들을 queue에 삽입한다.
+                queue.add(i);
+                d[i] = cost[i];
             }
         }
-        while (!noIndegrees.isEmpty()) {
-            int node = noIndegrees.poll();
-            System.out.print(node + " ");
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            *//*System.out.print(node + " ");*//*
             if (adjList[node] != null) {
                 for (int outNode : adjList[node]) {
+                    if (d[outNode] < d[node] + cost[outNode]) {
+                        d[outNode] = d[node] + cost[outNode];
+                    }
                     indegree[outNode]--;
-                    if(indegree[outNode]==0){
-                        noIndegrees.add(outNode);
+                    if (indegree[outNode] == 0) {
+                        queue.add(outNode);
                     }
                 }
             }
             adjList[node] = null;
         }
-    }
+        return d[goal];
+    }*/
 
-    private static void build(int goal) {
-        boolean[] visited = new boolean[1001];
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < queue.size(); i++) {
-                int curNode = queue.remove();
-                d[curNode] += cost[curNode];
-                if (adjList[curNode] != null) {
-                    for (int adjNode : adjList[curNode]) {
-                        if (visited[adjNode]) {
-                            // 다른 node들에 의해 이미
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
